f20801df170caaa2f09f23fcfc2f2fa6baed8959
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-21 15:50:37 +0530

    Updated code files: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 
 import CheckoutPage from "./pages/CheckoutPage";
@@ -17,6 +18,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
 <QueryClientProvider client={queryClient}>
+  <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <TooltipProvider>
     <Toaster />
     <Sonner />
@@ -36,6 +38,7 @@ const App = () => (
         </Routes>
     </BrowserRouter>
     </TooltipProvider>
+  </ThemeProvider>
 </QueryClientProvider>
 );
 
